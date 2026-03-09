543aee9eef481d265120ed40ff8168b5cf638ce9
Alef Farias <alef.alves1800@gmail.com>
2026-03-09 12:29:15 -0300

    Refactor deployment workflow and Flask app structure for Azure VM compatibility

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Renderiza o HTML da pasta /templates
+def hello():
     return """<h1>Hello World!</h1><p>Se voce esta vendo isso o CI/CD funcionou!</p>"""
 
 if __name__ == '__main__':
-    # Rodando em 0.0.0.0 para ser acessível dentro do container Docker
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port = 5000)
